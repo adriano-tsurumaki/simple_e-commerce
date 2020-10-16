10f18705cd271f3df7d4ae61b00b630b9fe3c22f
Adriano <adrianotsurumaki@gmail.com>
2020-10-15 23:59:33 -0300

    Added component rating star

--- web/src/components/ListingBooks/index.js
+++ web/src/components/ListingBooks/index.js
@@ -6,6 +6,27 @@ import { BsStar, BsStarHalf, BsStarFill } from "react-icons/bs";
 
 import './styles.css';
 
+const StarsRating = value => {
+    const rating = parseFloat(value.rating);
+    let starFill = Math.trunc(rating);
+    let starHalf = rating % 1;
+    let starEmpty = (5 - rating) < 0 ? 0 : (Math.trunc(5 - rating));
+
+    return (
+        <div className="card-star">
+            {starFill > 0 && [...Array(starFill)].map((value, index) => {
+                return <BsStarFill className="star-fill" key={index} />
+            })}
+
+            {starHalf > 0 && <BsStarHalf className="star-half" />}
+            
+            {starEmpty > 0 && [...Array(starEmpty)].map((value, index) => {
+                return <BsStar className="star" key={index} />
+            })}
+        </div>
+    )
+}
+
 const Release = () => {
     return (
         <h1>Release</h1>
@@ -25,27 +46,23 @@ const BestOffer = () => {
 }
 
 const BestSeller = () => {
-    useEffect(() => {
-        // Axios here
-        /*
-            {
-                title;
-                author;
-                price;
-                star;
-            }
-        */
-    }, []);
+    // useEffect(() => {
+    //     // Axios here
+    //     /*
+    //         {
+    //             title;
+    //             author;
+    //             price;
+    //             star;
+    //         }
+    //     */
+    // }, []);
 
     return (
         <>
             <div className="card-best-book">
                 <img src="https://images-na.ssl-images-amazon.com/images/I/51kcJqp-3-L._SX346_BO1,204,203,200_.jpg" alt="Book 1"/>
-                <div className="card-star">
-                    <BsStar className="star" />
-                    <BsStarHalf className="star-half" />
-                    <BsStarFill className="star-fill" />
-                </div>
+                <StarsRating rating="4.1" />
                 <div className="card-hidden">
                     
                 </div>
